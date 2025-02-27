e2ebb43c35b67d09ee11accad4317c32a2ea6c95
Dhruv Shelke <dhruvshelke588@gmail.com>
2025-02-27 16:51:24 -0600

    setup project db

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1>AI APP GEN</h1>
+      <h1 className="text-3xl font-bold underline">AI APP GEN</h1>
+      <Button>Click</Button>
     </div>
   );
 }
